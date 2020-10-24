ba583243cf7309f86e1f38356696eeccbf0c7686
Eduardo <eduardoariel2016@outlook.com>
2020-10-24 19:21:29 -0300

    trabajos de hash

---
+++ CTR/CTR/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace CTR
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			string[] Salida={"0011","0100","0101","0110","0111","1001","1101","1111","1100","1110","1011","1011","1000","0001","0010","0000"};
+			string mensaje="";
+			string key="1011";
+			string Ms_Parte1="";
+			string Ms_Parte2="";
+			int cont=0;
+			string k_ms1="";
+			string k_ms2="";
+			
+			
+			Console.WriteLine("Ingrese un numero binario de 8 bits:");
+			mensaje= Console.ReadLine();
+			Verificar(mensaje,8);
+			Console.WriteLine("Ingres un numero binario de 4 bits:");
+			key= Console.ReadLine();
+			Verificar(key,4);
+			
+			Ms_Parte1=mensaje.Substring(0,4);
+			Ms_Parte2=mensaje.Substring(4,4);
+			//Cuenta XOR (segunda parte del mensaje con la key)
+			for (int z = key.Length-1;z>=0 ; z--) {
+				for (int c = Ms_Parte2.Length-1; c >=0; c--) {
+					if (cont<=key.Length) {
+						if (key[z]==Ms_Parte2[c]) {
+							k_ms2="0"+k_ms2;
+							z--;
+						}
+						else{
+							k_ms2="1"+k_ms2;	
+							z--;
+						}
+					}
+				}
+			}
+			
+			//Cuenta XOR (primera parte del mensaje con la key)
+			for (int z = key.Length-1;z>=0 ; z--) {
+				for (int c = Ms_Parte1.Length-1; c >=0; c--) {
+					if (cont<=key.Length) {
+						if (key[z]==Ms_Parte1[c]) {
+							k_ms1="0"+k_ms1;
+							z--;
+						}
+						else{
+							k_ms1="1"+k_ms1;	
+							z--;
+						}
+					}
+				}
+			}
+			Console.WriteLine("mi primer bloque es ");
+			Console.WriteLine("Mi primer bloque es :"+k_ms1);
+			Bin_a_Des(k_ms1,Salida);
+			Console.WriteLine("\nMi segundo bloque es ");
+			Console.WriteLine("Mi segundo bloque es :"+k_ms2 );
+			Bin_a_Des(k_ms2,Salida);
+			Console.ReadKey(true);
+		}
+		public static void Verificar(string Ms, int n){
+			for (int x = Ms.Length-1 ; x >=0; x++) {
+				if (Ms[x]=='0' || Ms[x]=='1') {
+					if (Ms.Length<n) {
+						Console.WriteLine("El mensaje es menor(tiene pocos bits)");
+						Console.WriteLine("INGRESE EL DATO DE NUEVO");
+						Ms=Console.ReadLine();
+						Verificar(Ms,n);
+					}
+					if (Ms.Length==n) {
+						Console.WriteLine("los datos son correctos\n-----------------------------");
+						break;	
+					}
+					else{
+						Console.WriteLine("El mensaje es mayor(tiene mas bits)");
+						Console.WriteLine("INGRESE EL DATO DE NUEVO");
+						Ms=Console.ReadLine();
+						Verificar(Ms,n);
+					}
+				} 
+				else{
+					Console.WriteLine("El mensaje no es binario");
+					Console.WriteLine("INGRESE EL DATO DE NUEVO");
+					Ms=Console.ReadLine();
+					Verificar(Ms,n);
+					break;
+				}
+			}
+		}
+		
+		public static void Bin_a_Des(string Ms, string[] salida){
+			bool V=true;
+			int i=0;
+			string f="";
+			
+			for (int x = Ms.Length-1,y=0; x >=0; x--,y++) {
+				if (Ms[x]=='0' || Ms[x]=='1') {
+					i+=(int)(int.Parse(Ms[x].ToString()) * Math.Pow(2,y));
+				}
+				else{
+					Console.WriteLine("El numero no es binario(Mensaje):");
+					V=false;
+					break;
+				}
+			}
+			if (V) {
+				//Console.WriteLine("El numero en decimal es: {0}",i);
+				Console.WriteLine("La salida de la tabla es: "+salida[i]);
+				
+			}
+		}
+		
+
+		
+		
+		
+		
+	}	
+}
---
+++ Mac/Mac/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace Mac
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			string Mensaje="";
+			string Key="";
+			string Ipad="1100";
+			string Opad="1001";
+			string K_Ipad="";
+			string k_Opad="";
+			int cont=0;
+			
+			
+			Console.WriteLine("Ingrese un numero binario de 8 bits:");
+			Mensaje= Console.ReadLine();
+			Console.WriteLine("Ingres un numero binario de 4 bits:");
+			Key= Console.ReadLine();
+			
+			//hace la cuentas de XOR(KEY XOR Ipad)
+			for (int x = Key.Length-1; x>=0 ; x--) {
+				for (int y = Ipad.Length-1; y >=0; y--) {
+					if (cont<=Key.Length) {
+						if (Key[x]==Ipad[y]) {
+							K_Ipad="0"+K_Ipad;
+							x--;
+						}
+						else{
+							K_Ipad="1"+K_Ipad;	
+							x--;
+						}
+					}
+				}
+			}
+			//fin de la cuenta XOR(KEY xor Ipad)
+			
+			//Hace la cuentas XOR(KEY XOR Opad)			
+			for (int z = Key.Length-1;z>=0 ; z--) {
+				for (int c = Opad.Length-1; c >=0; c--) {
+					if (cont<=Key.Length) {
+						if (Key[z]==Opad[c]) {
+							k_Opad="0"+k_Opad;
+							z--;
+						}
+						else{
+							k_Opad="1"+k_Opad;	
+							z--;
+						}
+					}
+				}
+			}
+			//fin de la cuenta XOR(KEY xor Opad)
+			
+			Bin_a_Des(K_Ipad+Mensaje,k_Opad);
+			Console.ReadKey(true);
+		}
+		//------------------------------------
+		public static void Bin_a_Des(string Ms,string k_Opad){
+			bool V=true;
+			int i=0;
+			
+			for (int x = Ms.Length-1,y=0; x >=0; x--,y++) {
+				if (Ms[x]=='0' || Ms[x]=='1') {
+					i+=(int)(int.Parse(Ms[x].ToString()) * Math.Pow(2,y));
+				}
+				else{
+					Console.WriteLine("El numero no es binario(Mensaje):");
+					V=false;
+					break;
+				}
+			}
+			if (V) {
+				Console.WriteLine("El numero en decimal es: {0}",i);	
+				Hash(i, k_Opad);	
+			}
+		}//combierte de binario a decimal
+		//---------------------------------
+		public static void Hash(int i, string k_Opad){
+			int Resultado=0;
+			//int cont=0;
+			Resultado=(i*7)%13;
+			Console.WriteLine("El numero hasheado es(En base 10):"+Resultado);
+			Des_a_Bin(Resultado, k_Opad);
+		}//hace el calculo del hash (a*7)mod 13
+		//---------------------------------------------
+		public static void Des_a_Bin(int Resultado, string k_Opad){
+			string cifrado="";
+			bool v=true;
+			if (v) {
+				while (Resultado > 0) {
+				cifrado= Resultado%2+cifrado;
+				Resultado= Resultado/2;
+				}
+				if (cifrado.Length<4) {
+					Console.WriteLine("El Mensaje cifrado es: "+"0"+cifrado);
+					cifrado="0"+cifrado;
+					Console.WriteLine("\nSegunda parte");
+					v=false;
+					Bin_a_Des(k_Opad+cifrado,null);
+				}
+				else
+					Console.WriteLine("El Mensaje cifrado es: "+cifrado);
+					Console.WriteLine("Segunda parte");
+					Bin_a_Des(k_Opad+cifrado,null);
+				}
+			else 
+				Console.WriteLine("CIFRADO TERMINADO");
+		}//combierte de decimal a binario
+		//------------------------------
+	}
+}

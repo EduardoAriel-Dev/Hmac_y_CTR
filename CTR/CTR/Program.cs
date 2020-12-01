b4acdf5baadfd61eedde5038d18d0584be04adf3
Eduardo Montaño <eduardoariel2016@outlook.com>
2020-11-30 23:31:11 -0300

    Add files via upload

--- CTR/CTR/Program.cs
+++ CTR/CTR/Program.cs
@@ -6,14 +6,15 @@ namespace CTR
 	{
 		public static void Main(string[] args)
 		{
+				//entrada   	0  ,   1  ,   2	 ,   3  ,   4  ,   5  ,  6   ,   7  ,  8   ,  9   ,  10  ,  11  ,  12  ,  13  ,  14  ,  15  	  
 			string[] Salida={"0011","0100","0101","0110","0111","1001","1101","1111","1100","1110","1011","1011","1000","0001","0010","0000"};
 			string mensaje="";
 			string key="1011";
 			string Ms_Parte1="";
 			string Ms_Parte2="";
-			int cont=0;
-			string k_ms1="";
-			string k_ms2="";
+			string nose="01";
+			string contador="00";
+			string nose_cont=nose+contador;
 			
 			
 			Console.WriteLine("Ingrese un numero binario de 8 bits:");
@@ -25,45 +26,52 @@ namespace CTR
 			
 			Ms_Parte1=mensaje.Substring(0,4);
 			Ms_Parte2=mensaje.Substring(4,4);
-			//Cuenta XOR (segunda parte del mensaje con la key)
-			for (int z = key.Length-1;z>=0 ; z--) {
-				for (int c = Ms_Parte2.Length-1; c >=0; c--) {
-					if (cont<=key.Length) {
-						if (key[z]==Ms_Parte2[c]) {
-							k_ms2="0"+k_ms2;
-							z--;
-						}
-						else{
-							k_ms2="1"+k_ms2;	
-							z--;
-						}
-					}
-				}
-			}
 			
-			//Cuenta XOR (primera parte del mensaje con la key)
-			for (int z = key.Length-1;z>=0 ; z--) {
-				for (int c = Ms_Parte1.Length-1; c >=0; c--) {
-					if (cont<=key.Length) {
-						if (key[z]==Ms_Parte1[c]) {
-							k_ms1="0"+k_ms1;
-							z--;
-						}
-						else{
-							k_ms1="1"+k_ms1;	
-							z--;
-						}
-					}
-				}
-			}
-			Console.WriteLine("mi primer bloque es ");
-			Console.WriteLine("Mi primer bloque es :"+k_ms1);
-			Bin_a_Des(k_ms1,Salida);
-			Console.WriteLine("\nMi segundo bloque es ");
-			Console.WriteLine("Mi segundo bloque es :"+k_ms2 );
-			Bin_a_Des(k_ms2,Salida);
+			//cifrado del primer mensaje
+			Console.WriteLine("Mi primer bloque es ");
+			Console.WriteLine("La primera parte del bloque es: "+Ms_Parte1);
+			string x=cuenta_XoR(key,nose_cont);
+			string y=Bin_a_Des(x,Salida);
+			Console.WriteLine("Mi primer bloque cifrado es: "+cuenta_XoR(y,Ms_Parte1));
+			Console.WriteLine("los datos son correctos\n-----------------------------");
+			//Fin del Primer cifrado
+			
+			//Cifrado del segundo bloque
+			Console.WriteLine("Mi segundo bloque es ");
+			Console.WriteLine("La segunda parte del bloque es: "+Ms_Parte2);
+			nose+=Contador(contador);//llamo al contador
+			string z=cuenta_XoR(key,nose);
+			string q=Bin_a_Des(z,Salida);
+			Console.WriteLine("Mi segundo bloque cifrado es: "+cuenta_XoR(q,Ms_Parte2));
+			Console.WriteLine("los datos son correctos\n-----------------------------");
+			//Fin del Segundo cifrado
+			
+			
+			
+
 			Console.ReadKey(true);
 		}
+		
+		public static string cuenta_XoR(string a,string b){
+			int cont=0;
+			string nose1="";
+			for (int z = a.Length-1;z>=0 ; z--) {
+				for (int c = b.Length-1; c >=0; c--) {
+					if (cont<=a.Length) {
+						if (a[z]==b[c]) {
+							nose1="0"+nose1;
+							z--;
+						}
+						else{
+							nose1="1"+nose1;	
+							z--;
+						}
+					}
+				}
+			}
+			return nose1;
+		}//Función de las cuentas XoR
+		
 		public static void Verificar(string Ms, int n){
 			for (int x = Ms.Length-1 ; x >=0; x++) {
 				if (Ms[x]=='0' || Ms[x]=='1') {
@@ -92,9 +100,9 @@ namespace CTR
 					break;
 				}
 			}
-		}
+		}//Verifica que los datos sean correctos
 		
-		public static void Bin_a_Des(string Ms, string[] salida){
+		public static string Bin_a_Des(string Ms, string[] salida){
 			bool V=true;
 			int i=0;
 			string f="";
@@ -110,16 +118,33 @@ namespace CTR
 				}
 			}
 			if (V) {
-				//Console.WriteLine("El numero en decimal es: {0}",i);
 				Console.WriteLine("La salida de la tabla es: "+salida[i]);
-				
+				f=salida[i];
 			}
-		}
+			return f;
+		}//Pasa de Bits a Decimales
 		
-
+		public void des_a_bin(int Resultado){
+		string cifrado="";
+			bool v=true;
+			if (v) {
+				while (Resultado > 0) {
+				cifrado= Resultado%2+cifrado;
+				Resultado= Resultado/2;
+				}	
+			}
+		}//Pasa de Decimales a Bits
 		
-		
-		
+		public static string Contador(string contador){
+			String output = Convert.ToString(Convert.ToInt32(contador, 2), 10); 
+			int result = Int32.Parse(output)+1;
+			string binario = Convert.ToString(result, 2);
+			if (binario=="01" | binario=="10" | binario=="00" | binario=="11") {
+				return binario;
+			}
+			else
+				return "0"+binario;
+		}//Contador
 		
 	}	
 }
